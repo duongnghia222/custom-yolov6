67b5dd045862fda0e58a215923b3a9e08c7352f5
duongnghia222 <duongnghia222@gmail.com>
2023-12-01 11:58:11 +0700

    speech recognition but..

---
+++ tools/speech_recog.py
@@ -0,0 +1,33 @@
+import speech_recognition as sr
+
+# Initialize recognizer
+r = sr.Recognizer()
+
+# Using the default microphone as the audio source
+with sr.Microphone() as source:
+    print("Please say something:")
+    # Adjust the recognizer sensitivity to ambient noise
+    r.adjust_for_ambient_noise(source, duration=1)
+    # Listening for the first phrase and extracting it into audio data
+    audio = r.listen(source)
+
+try:
+    # Using Google Web Speech API to recognize audio
+    print("Google Speech Recognition thinks you said:")
+    print(r.recognize_google(audio))
+except sr.UnknownValueError:
+    # API was unable to understand the audio
+    print("Google Speech Recognition could not understand the audio")
+except sr.RequestError as e:
+    # API was unreachable or unresponsive
+    print(f"Could not request results from Google Speech Recognition service; {e}")
+
+
+try:
+    # Use Sphinx for offline speech recognition
+    print("Sphinx thinks you said:")
+    print(r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand the audio")
+except sr.RequestError as e:
+    print(f"Sphinx error; {e}")
